a9d8cbe997d1eee0aea90b0aeb3ede89172bc0e5
Lukas Gjetting <lukasgjetting@gmail.com>
2018-09-26 21:22:36 +0200

    Set up test scenario

--- src/App.js
+++ src/App.js
@@ -1,18 +1,49 @@
 import React, { Component } from 'react';
+import SlideDown from 'react-slidedown';
+import 'react-slidedown/lib/slidedown.css';
 import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      page: 0,
+      random: 0,
+    };
+  }
+
+  setPage(pageNumber) {
+    this.setState({ page: pageNumber });
+    setTimeout(() => {
+      this.setState({ page: pageNumber + 1 });
+    }, 500 * Math.random());
+  }
   render() {
+    const { page, random } = this.state;
+
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          {random}
+          <h1 className="App-title" onClick={() => this.setPage(page + 1)}>Click me to change page!</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <SlideDown>
+          { page % 2 === 0 &&
+            <p className="page blue">
+              Page even!
+            </p>
+          }
+        </SlideDown>
+        <SlideDown>
+          { page % 2 === 1 &&
+            <p className="page red">
+              Page odd!
+            </p>
+          }
+        </SlideDown>
       </div>
     );
   }
